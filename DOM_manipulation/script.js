23fff39c805f92b980c8ee47aab1e060fa831b9b
021_MohitKandera <mohit.krishanwal9557@gmail.com>
2026-01-24 02:14:18 -0800

    Add files via upload

---
+++ DOM_manipulation/script.js
@@ -0,0 +1,70 @@
+// console.log("Script loaded");
+
+
+// change DOM manipulation in ,Mohit
+//document.body.childNodes[1].innerText = "Mohit" 
+
+
+
+// How access the element by id
+// let heading = document.getElementById("heading");
+// console.log(heading);
+
+
+
+// How access the element by class
+// let headings = document.getElementsByClassName("heading2");
+// console.log(headings);
+
+
+
+ // access element of button by id
+// let btn = document.getElementById("mybtn");
+//  console.log(btn);
+
+
+
+
+// access element of button by tag name
+// let paragraph = document.getElementsByTagName('p');
+// console.log(paragraph);
+
+
+// access element of h1 by query selector
+// let heading1 = document.querySelector('h1');  // 1st h1 element
+// console.log(heading1);
+
+
+// let headingAll = document.querySelectorAll('h1');  // all h1 element
+// console.log(headingAll);
+
+
+
+// let heading = document.querySelector('.heading2');  // element access by class = .
+// console.log(heading);
+
+
+// let heading = document.querySelector('#heading');  // element access by id = #
+// console.log(heading);
+
+
+
+// tagName = return tag for element nodes
+// innerText = return text inside the element
+// innerHTML = return text with html tags inside the element  , tags = <p>, <div> , <h1> ,etc
+// textContent = return text inside the element including hidden text
+
+
+
+// const children = document.querySelector('div').children  // return children of div element = p , div
+// console.log(children);
+
+
+
+// const heading = document.querySelector('h3')
+// console.log(heading)
+
+
+
+
+
