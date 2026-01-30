4d8894af67953d5358689f2759254af2fad60c5a
021_MohitKandera <mohit.krishanwal9557@gmail.com>
2026-01-30 05:44:38 -0800

    Add files via upload

---
+++ Selectors/script.js
@@ -0,0 +1,18 @@
+
+// // getElemnetById
+// const elementbyId = document.getElementById("MyId")
+// console.log(elementbyId.innerHTML);
+
+
+// //getElementsByClassName
+// const elements = document.getElementsByClassName("MyClass")
+// for(let i = 0; i<elements.length; i++){
+//     console.log(elements[i].textContent);
+// }
+
+ 
+// getElementByTagName
+const elementsTag = document.getElementsByTagName("div")
+for(let i = 0; i<elementsTag.length; i++){
+    console.log(elementsTag[i].textContent);
+}
